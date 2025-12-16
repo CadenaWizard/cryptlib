5b8a1986124092796dec692d91eaf326924d7722
optout <13562139+optout21@users.noreply.github.com>
2025-12-16 19:34:26 +0100

    Python sample app fix

--- samples/python/main.py
+++ samples/python/main.py
@@ -1,4 +1,4 @@
-import dlccryptlib
+import dlccryptlib_py
 
 def sample():
     print("Dlccryptlib sample app, Python")
@@ -6,14 +6,14 @@ def sample():
     entropy_hex = "99d33a674ce99d33a674ce99d33a674c" # oil x 12
     network = "signet";
 
-    xpub = dlccryptlib.init_with_entropy(entropy_hex, network)
+    xpub = dlccryptlib_py.init_with_entropy(entropy_hex, network)
     print(f"Library initialized, xpub {xpub}")
 
-    pubkey0 = dlccryptlib.get_public_key(0)
+    pubkey0 = dlccryptlib_py.get_public_key(0)
     print(f"pubkey 0: {pubkey0}")
 
     hash = "0001020300000000000000000000000000000000000000000000000000010203"
-    sig = dlccryptlib.sign_hash_ecdsa(hash, 0, pubkey0)
+    sig = dlccryptlib_py.sign_hash_ecdsa(hash, 0, pubkey0)
     print(f"signature: {sig}")
 
 if __name__ == "__main__":
