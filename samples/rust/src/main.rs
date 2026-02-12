f9bb99c8fe8a17fdc8b0bf7f3991a73b893c2642
optout <13562139+optout21@users.noreply.github.com>
2026-02-12 14:30:23 +0100

    Adapt samples

--- samples/rust/src/main.rs
+++ samples/rust/src/main.rs
@@ -16,10 +16,10 @@ fn main() {
     let xpub = dlccryptlib::init_with_entropy(entropy_hex, network).unwrap();
     println!("xpub: {xpub}");
 
-    let pubkey0 = dlccryptlib::get_public_key(0).unwrap();
+    let pubkey0 = dlccryptlib::get_public_key(0, 0).unwrap();
     println!("pubkey 0: {pubkey0}");
 
     let hash = "0001020300000000000000000000000000000000000000000000000000010203";
-    let sig = dlccryptlib::sign_hash_ecdsa(hash, 0, &pubkey0).unwrap();
+    let sig = dlccryptlib::sign_hash_ecdsa(hash, 0, 0, &pubkey0).unwrap();
     println!("signature: {sig}");
 }
